774ef0ecb2206339e001d04f45c3c0650a502445
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-19 15:08:03 +0530

    Implements Service Layer

---
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Customer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @PostMapping("/add")
+    public void addCustomer(Customer customer) {
+
+    }
+
+}
---
+++ src/main/java/edu/icet/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,11 @@
+package edu.icet.service.impl;
+
+import edu.icet.dto.Customer;
+import edu.icet.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void addCustomer(Customer customer) {
+
+    }
+}
