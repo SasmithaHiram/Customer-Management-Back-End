603ee70e858bdc5200b69f65dd818cf8f08a7aca
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-20 07:25:35 +0530

    Inject ModelMapper

--- src/main/java/edu/icet/service/impl/CustomerServiceImpl.java
+++ src/main/java/edu/icet/service/impl/CustomerServiceImpl.java
@@ -1,11 +1,24 @@
 package edu.icet.service.impl;
 
 import edu.icet.dto.Customer;
+import edu.icet.entity.CustomerEntity;
+import edu.icet.repository.CustomerRepository;
 import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 
 public class CustomerServiceImpl implements CustomerService {
+
+    final CustomerRepository repository;
+    final ModelMapper mapper;
+
     @Override
     public void addCustomer(Customer customer) {
-
+        repository.save(mapper.map(customer, CustomerEntity.class));
     }
+
 }
